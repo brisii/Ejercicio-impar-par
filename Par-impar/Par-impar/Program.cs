33abd170ae8698ad338e69f2d5f077c65fa2a8ac
BJimenez <jjimenezu@miumg.edu.gt>
2023-03-04 11:21:29 -0800

    jenny

---
+++ Par-impar/Par-impar/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Escribe un número: ");
+long numero = Convert.ToInt64(Console.ReadLine());
+long sobrante = numero % 2;
+if (sobrante == 0)
+{
+    Console.WriteLine("El número es par");
+}
+else
+{
+    Console.WriteLine("El número es impar");
+}
